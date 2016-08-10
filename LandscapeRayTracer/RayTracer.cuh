af34336d6c5f7aef96daa7373bb5f80bff07871f
Jean Paul <jeanfilho@gmail.com>
2016-08-10 12:54:29 +0200

    In progress

---
+++ LandscapeRayTracer/RayTracer.cuh
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <glm\glm.hpp>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+#include <helper_cuda.h>
+
+
+__global__ void allocateGrids(bool** coarse_grid, bool* grid, bool **d_coarse_grid, bool* d_grid)
+{
+
+}
+
+
+
